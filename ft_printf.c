fd577720a49a855087035df96df6dc37ec191e72
clde-ber <clde-ber@student.42.fr>
2021-07-18 07:48:23 +0200

    tests

--- ft_printf.c
+++ ft_printf.c
@@ -14,6 +14,7 @@ void    revstr(char **str)
         (*str)[len - 1 - i] = tmp[i];
         i++;
     }
+    free_string(tmp);
 }
 
 char *ft_strdup(char *str)
@@ -108,6 +109,14 @@ int len_long(unsigned long long n)
     return (i);
 }
 
+int ft_isspace(char c)
+{
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v' || \
+    c == '\f')
+        return (1);
+    return (0);
+}
+
 char    *ft_itoa(int n)
 {
     int i;
@@ -115,14 +124,13 @@ char    *ft_itoa(int n)
     int count;
     int res;
     char *str;
+    int len;
 
     i = 0;
     neg = 0;
     count = ft_pow(10, len_int(n));
-    if (n < 0)
-        str = malloc(sizeof(char) * (len_int(n) + 2));
-    else
-        str = malloc(sizeof(char) * (len_int(n) + 1));
+    len = (n < 0) ? len_int(n) + 2 : len_int(n) + 1;
+    str = malloc(sizeof(char) * (len + 1));
     if (!(str))
         return (0);
     res = n;
@@ -135,7 +143,7 @@ char    *ft_itoa(int n)
         n = -n;
         res = n;
     }
-    while (res % count)
+    while (i < len - 1)
     {
         str[i] = res / count + '0';
         res = n % count;
@@ -194,10 +202,10 @@ char *to_hex(unsigned long long n, char *base)
     count = ft_pow(10, len_long(n));
     res = n;
     k = n;
-    str = malloc(sizeof(char) * (len_long(n) + 1));
+    str = malloc(sizeof(char) * (len_long(n) + 2));
     if (!(str))
         return (0);
-    while (n > 9)
+    while (n / len)
     {
         str[i] = base[n % len];
         n = n / len;
@@ -215,84 +223,243 @@ void    init_struct_flags(t_flag *flags)
     flags->prec = 0;
     flags->set_width = 0;
     flags->width = 0;
+    flags->minus = 0;
+    flags->nb = 0;
 }
 
-void    print_str(char *str, t_flag *flags)
+void    update_flags_null(t_flag *flags, char *str, int len)
+{
+    int limit;
+
+    limit = ft_strlen(str);
+    if (flags->set_prec)
+    {
+        if (flags->prec >= len)
+            flags->prec = 0;
+        else
+        {
+            str[0] = '\0';
+            flags->prec = 0;
+        }
+    }
+    if (flags->prec < 0)
+        flags->prec = 0;
+    if (flags->set_width)
+    {
+        if (flags->width > ft_strlen(str))
+            flags->width = flags->width - flags->prec - ft_strlen(str);
+        else
+            flags->width = 0;
+    }
+}
+
+void    update_flags_nb(t_flag *flags, char *str, int len)
+{
+    int limit;
+
+    limit = (flags->minus) ? ft_strlen(str) - 1 : ft_strlen(str);
+    if (flags->set_prec)
+    {
+        if (flags->prec > len)
+            flags->prec = flags->prec - limit;
+        else
+        {
+            str[flags->prec] = '\0';
+            flags->prec = 0;
+        }
+    }
+    if (flags->prec < 0)
+        flags->prec = 0;
+    if (flags->set_width)
+    {
+        if (flags->width > ft_strlen(str))
+            flags->width = flags->width - flags->prec - ft_strlen(str);
+        else
+            flags->width = 0;
+    }
+}
+
+void    update_flags_str(t_flag *flags, char *str, int len)
+{
+    int limit;
+
+    limit = ft_strlen(str);
+    if (flags->set_prec)
+    {
+        if (flags->prec > len)
+            flags->prec = flags->prec - limit;
+        else
+        {
+            str[flags->prec] = '\0';
+            flags->prec = 0;
+        }
+    }
+    if (flags->prec < 0)
+        flags->prec = 0;
+    if (!flags->set_width || (flags->set_width && flags->width < ft_strlen(str)))
+        flags->prec = 0;
+    if (flags->set_width)
+    {
+        if (flags->width >= ft_strlen(str))
+            flags->width = flags->width - flags->prec - ft_strlen(str);
+        else
+            flags->width = 0;
+    }
+}
+
+void    update_flags(t_flag *flags, char *str)
+{
+    if (flags->nb)
+        update_flags_nb(flags, str, 0);
+    else if (!(ft_strcmp(str, "(null)") == 0 || \
+    ft_strcmp(str, "") == 0))
+        update_flags_str(flags, str, ft_strlen(str));
+    else
+        update_flags_null(flags, str, ft_strlen(str));
+}
+
+void    print_str(char *str, t_flag *flags, int *ret, int minus)
 {
     int len;
     int i;
+    int min;
+    int cmp;
 
     len = 0;
     i = 0;
-    if (flags->set_prec == 1)
-        len = flags->prec;
-    else
-        len = ft_strlen(str);
-    while (i < flags->width - flags->prec && flags->width > flags->prec)
+    min = 0;
+    cmp = 0;
+    update_flags(flags, str);
+    while (i < flags->width)
     {
         write(1, " ", 1);
         i++;
     }
-    i = 0;
-    while (i < flags->prec - ft_strlen(str) && flags->prec > ft_strlen(str))
+    if (flags->minus == 1)
     {
-        write(1, "0", 1);
+        write(1, &str[0], 1);
+        min = 1;
+    }
+    *ret += i;
+    i = 0;
+    while (i < flags->prec)
+    {
+        if (flags->nb)
+            write(1, "0", 1);
+        else
+            write(1, " ", 1);
         i++;
     }
+    *ret += i;
     i = 0;
-    while (i < len)
+    while (str[i + min])
     {
-        write(1, &str[i], 1);
+        write(1, &str[i + min], 1);
         i++;
     }
+    *ret = *ret + i + min;
 }
 
 char    *find_conv(int *i, const char *format, va_list *args, t_flag *flags)
 {
     char *ret;
+    char   *tmp;
+    int     is_neg;
 
     ret = NULL;
+    tmp = NULL;
     if (format[*i] == 'd')
-        ret = ft_itoa(va_arg(*args, int));
+    {
+        is_neg = va_arg(*args, int);
+        if (is_neg < 0)
+            flags->minus = 1;
+        flags->nb = 1;
+        ret = ft_itoa(is_neg);
+    }
     if (format[*i] == 's')
-        ret = ft_strdup(va_arg(*args, char *));
+    {
+        if (!(tmp = va_arg(*args, char *)))
+            ret = ft_strdup("(null)");
+        else
+            ret = ft_strdup(tmp);
+    }
     if (format[*i] == 'x')
+    {
+        flags->nb = 1;
         ret = to_hex(va_arg(*args, unsigned long long), "0123456789abcdef");
-    *i = ft_strlen(ret);
+    }
+    (*i)++;
     return (ret);
 }
 
-void    find_flags(int *i, const char *format, va_list *args, t_flag *flags)
+void    find_flags(int *i, const char *format, t_flag *flags, va_list *args)
 {
-    if (ft_isdigit(format[*i]))
+    if (ft_isdigit(format[*i]) || format[*i] == '+' || format[*i] == '-')
     {
         flags->set_width = 1;
         flags->width = ft_atoi((char *)&format[*i]);
+        if (format[*i] == '+' || format[*i] == '-')
+            (*i)++;
+        while (ft_isdigit(format[*i]))
+            (*i)++;
     }
-    while (ft_isdigit(format[*i]))
+    else if (format[*i] == '*')
+    {
+        flags->set_width = 1;
+        flags->width = va_arg(*args, int);
         (*i)++;
+    }
     if (format[*i] == '.')
     {
         flags->set_prec = 1;
-        flags->prec = ft_atoi((char *)&format[++(*i)]);
+        if (ft_isdigit(format[++(*i)]))
+            flags->prec = ft_atoi((char *)&format[*i]);
+        if (format[*i] == '+' || format[*i] == '-')
+            (*i)++;
+        while (ft_isdigit(format[*i]))
+            (*i)++;
     }
-    while (ft_isdigit(format[*i]))
+    if (format[*i] == '*')
+    {
+        flags->prec = va_arg(*args, int);
         (*i)++;
+    }
 }
 
-void    parse_string(va_list *args, const char *format, int *ret, t_flag *flags)
+void    free_string(char *str)
+{
+    free(str);
+    str = NULL;
+}
+
+int    parse_string(va_list *args, const char *format, int *ret, t_flag *flags)
 {
     int i;
     char *str;
 
     i = 0;
     str = NULL;
-    while (format[i] != '%' || (format[i] == '%' && format[i + 1] == '%'))
+    init_struct_flags(flags);
+    if (format[0] == '\0')
+        return (0);
+    while (format[i] && (format[i] != '%' || (format[i] == '%' && format[i + 1] == '%')))
+    {
+        write(1, &format[i], 1);
         i++;
-    *ret = ++i;
-    find_flags(&i, format, args, flags);
+    }
+    *ret += i;
+    if (format[i] == '\0')
+        return (0); 
+    i++;
+    find_flags(&i, format, flags, args);
     str = find_conv(&i, format, args, flags);
-    print_str(str, flags);
+    if (!(str))
+        return (-1);
+    print_str(str, flags, ret, flags->minus);
+    free_string(str);
+    if (i < ft_strlen((char *)format))
+        parse_string(args, &format[i], ret, flags);
+    return (0);
 }
 
 int ft_printf(const char *format, ...)
@@ -304,23 +471,59 @@ int ft_printf(const char *format, ...)
     ret = 0;
     init_struct_flags(&flags);
     va_start(args, format);
-    parse_string(&args, format, &ret, &flags);
+    if (parse_string(&args, format, &ret, &flags) == -1)
+        ret = -1;
     va_end(args);
-    return (0);
+    return (ret);
 }
 /*
 int main(void)
 {
-    printf("str %s\n", ft_itoa(-12345));
-    printf("%d\n", ft_atoi("-12345"));
-    printf("str %s\n", ft_itoa(-2147483648));
-    printf("%d\n", ft_atoi("-2147483648"));
-    printf("str %s\n", ft_itoa(2147483647));
-    printf("%d\n", ft_atoi("2147483647"));
-    printf("hex %s\n", to_hex(2147483647, "0123456789abcdef"));
-    printf("hex %s\n", to_hex(1234, "0123456789abcdef"));
-    printf("hex %s\n", to_hex(17, "0123456789abcdef"));
-    ft_printf("%05.02d\n", 12345);
+    printf("%d\n", printf("%8.5x", 0));
+    printf("%d\n", ft_printf("%8.5x", 0));
+    printf("%d\n", printf("%03.3d", -8462));
+    printf("%d\n", ft_printf("%03.3d", -8462));
+    printf("%d\n", printf("%03.3d", 6983));
+    printf("%d\n", ft_printf("%03.3d", 6983));
+    printf("%d\n", printf("%08.5d", 34));
+    printf("%d\n", ft_printf("%08.5d", 34));
+    printf("%d\n", printf("%32s", "abc"));
+    printf("%d\n", ft_printf("%32s", "abc"));
+    printf("%d\n", printf("%16s", "nark nark"));
+    printf("%d\n", ft_printf("%16s", "nark nark"));
+    printf("%d\n", printf("%5s", "goes over"));
+    printf("%d\n", ft_printf("%5s", "goes over"));
+    printf("%d\n", printf("%.3d", 13862));
+    printf("%d\n", ft_printf("%.3d", 13862));
+    printf("%d\n", printf("%.4d", -2372));
+    printf("%d\n", ft_printf("%.4d", -2372));
+    printf("%d\n", printf("%.6d", -3));
+    printf("%d\n", ft_printf("%.6d", -3));
+    printf("%d\n", printf("%.09s", NULL));
+    printf("%d\n", ft_printf("%.09s", NULL));
+    printf("%d\n", printf("this %d number", 17));
+    printf("%d\n", ft_printf("this %d number", 17));
+    printf("%d\n", printf("%.5d", 2));
+    printf("%d\n", ft_printf("%.5d", 2));
+    printf("%d\n", printf("%.7s", "hello"));
+    printf("%d\n", ft_printf("%.7s", "hello"));
+    printf("%d\n", printf("%.3s%.2s", "holla", "bitch"));
+    printf("%d\n", ft_printf("%.3s%.2s", "holla", "bitch"));
+    printf("%d\n", printf("%20.6s", "hi low\0don't print me lol\0"));
+    printf("%d\n", ft_printf("%20.6s", "hi low\0don't print me lol\0"));
+    printf("%d\n", printf("%.4d", 5263));
+    printf("%d\n", ft_printf("%.4d", 5263));
+    printf("%d\n", printf("%.03s", NULL));
+    printf("%d\n", ft_printf("%.03s", NULL));
+    printf("%d\n", printf("%3.1s", NULL));
+    printf("%d\n", ft_printf("%3.1s", NULL));
+    printf("%d\n", printf("%9.1s", NULL));
+    printf("%d\n", ft_printf("%9.1s", NULL));
+    printf("%d\n", printf("%.5d", 2));
+    printf("%d\n", ft_printf("%.5d", 2));
+    printf("%d\n", printf("%.6d", -3));
+    printf("%d\n", ft_printf("%.6d", -3));
+    printf("%d\n", printf("%.3d", 0));
+    printf("%d\n", ft_printf("%.3d", 0));
     return (0);
-}
-*/
+}*/
--- ft_printf.h
+++ ft_printf.h
@@ -12,15 +12,27 @@ typedef struct s_flag
     int prec;
     int set_width;
     int width;
+    int minus;
+    int nb;
 }           t_flag;
 
-int ft_isdigit(char c);
-int ft_pow(int n, int pow);
-int len_int(int n);
-char    *ft_itoa(int n);
-int ft_atoi(char *str);
+void    free_string(char *str);
+void    revstr(char **str);
 char *ft_strdup(char *str);
 int ft_strcmp(char *s1, char *s2);
 int ft_strlen(char *str);
+int ft_isdigit(char c);
+int ft_pow(int n, int pow);
+int len_int(int n);
+int len_long(unsigned long long n);
+char    *ft_itoa(int n);
+int ft_atoi(char *str);
+char *to_hex(unsigned long long n, char *base);
+void    init_struct_flags(t_flag *flags);
+void    print_str(char *str, t_flag *flags, int *ret, int minus);
+char    *find_conv(int *i, const char *format, va_list *args, t_flag *flags);
+void    find_flags(int *i, const char *format, t_flag *flags, va_list *args);
+int    parse_string(va_list *args, const char *format, int *ret, t_flag *flags);
+int ft_printf(const char *format, ...);
 
 #endif
